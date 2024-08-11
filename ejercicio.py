bfcc97c0eb055ce262c58fe6a61d55145cc30033
Jimmy David Cevallos Zambrano <jimdav1506ceva@gmail.com>
2024-08-11 17:55:48 -0500

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -9,6 +9,3 @@ print(f"Los productos en la lista de compras son: {productos}")
 # Convertir la lista de compras en una tupla
 def convertir_lista_a_tupla(lista):
     return tuple(lista) # Utiliza una función para convertir 
-
-tupla_productos = convertir_lista_a_tupla(productos)
-print(f"La tupla convertido en producto es: {tupla_productos}")
